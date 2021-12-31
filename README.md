498e6ab0f6cdebc2cf51901b6eed5e58d423c660
Thorsten Walk <darkiop@gmail.com>
2021-12-31 14:35:27 +0100

    init #2

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # iac-ansible
 ansible-playbook2
+
+# Software
+
+group_vars/all/vars.yml
+default_software:
+additional_software:
+# Passwords
+
+group_vars/all/vault
+
+vault_user_password: <password>
+vault_user_email_password: <password>
+vault_user_samba_password: <password>
+
+# SSH Public Keys
+files/ssh/<username>.key.pub
--- files/scripts/dotfiles/dotfiles-install.sh
+++ files/scripts/dotfiles/dotfiles-install.sh
@@ -3,7 +3,6 @@
 # install
 dir=~/dotfiles
 files="bashrc gitconfig inputrc bash_profile dircolors"
-folders="byobu"
 
 # delete old symlinks
 for file in $files; do
@@ -12,19 +11,9 @@ for file in $files; do
     rm ~/.$file
   fi
 done
-for folder in $folders; do
-  if [ -d ~/.$folder ]; then
-    echo "delete: ~/.$folder";
-    rm -r ~/.$folder
-  fi
-done
 
 # new symlinks for files and folders
 for file in $files; do
   echo "create: ~/.$file"
   ln -s $dir/$file ~/.$file
-done
-for folder in $folders; do
-  echo "create: ~/.$folder"
-  ln -s $dir/$folder ~/.$folder
 done
