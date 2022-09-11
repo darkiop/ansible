58323d18281bc84369669dc27d771a810bd60716
Thorsten Walk <darkiop@gmail.com>
2022-09-11 12:37:53 +0200

    proxmox storage script

--- files/hosts/odin/proxmox-deactivate-storage.sh
+++ files/hosts/odin/proxmox-deactivate-storage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh pve01 -p 22 -l darkiop -t "sudo /usr/sbin/pvesm set --disable true odin-nfs-pve"
-ssh pve01 -p 22 -l darkiop -t "sudo /usr/sbin/pvesm set --disable true odin-pbs-pve"
+ssh darkiop@pve01 bash -c "'sudo /usr/sbin/pvesm set --disable true odin-nfs-pve'"
+ssh darkiop@pve01 bash -c "'sudo /usr/sbin/pvesm set --disable true odin-pbs-pve'"
 
 # EOF
