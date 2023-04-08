b9f540743524a8904b701bf9af39c06d202bd952
darkiop <darkiop@gmail.com>
2023-04-08 06:48:24 +0200

    ser2net.yaml

--- files/hosts/pve-ct-iobroker/iobroker-backup-sync-mariadb.sh
+++ files/hosts/pve-ct-iobroker/iobroker-backup-sync-mariadb.sh
@@ -7,38 +7,18 @@
 MNT="/mnt/odin/backup"
 BACKUPS="/opt/iobroker/backups/"
 RSYNC="sudo rsync -avz --exclude=yahka.* --exclude=homematic_* --exclude=grafana_* --exclude=javascripts_* --exclude=iobroker_* --exclude=redis*  --exclude=*-migration --delete $BACKUPS $MNT/mariadb"
-IOB_SIMPLEAPI_DOMAIN="pve-vm-iobroker"
-IOB_SIMPLEAPI_PORT="8087"
-OID_EMAIL_TITLE="javascript.0.System.SendeTextperEmailBetreff"
-OID_EMAIL_TEXT="javascript.0.System.SendeTextperEmail"
-EMAIL_TITLE="ioBroker%20Backup%20(mariadb)"
-EMAIL_TEXT_TRUE="iobroker-backup-sync-iobroker-mariadb.sh%20wurde%20ausgefuehrt"
-EMAIL_TEXT_FALSE="iobroker-backup-sync-iobroker-mariadb.sh%20konnte%20nicht%20ausgefuehrt%20werden"
 
-# check mountpoint
 if mountpoint -q $MNT
-then
-  # mountpoint exists, run rsync
-  $RSYNC
-  sleep 2
-  curl http://$IOB_SIMPLEAPI_DOMAIN:$IOB_SIMPLEAPI_PORT/set/$OID_EMAIL_TITLE?value=$EMAIL_TITLE >/dev/null 2>&1
-  sleep 2
-  curl http://$IOB_SIMPLEAPI_DOMAIN:$IOB_SIMPLEAPI_PORT/set/$OID_EMAIL_TEXT?value=$EMAIL_TEXT_TRUE >/dev/null 2>&1
-else
-  # try to mount
-  mount $MNT
-  # check mountpoint again
-  if mountpoint -q $MNT
   then
-    # mountpoint exists, run rsync
     $RSYNC
   else
-    # exit
-    curl http://$IOB_SIMPLEAPI_DOMAIN:$IOB_SIMPLEAPI_PORT/set/$OID_EMAIL_TITLE?value=$EMAIL_TITLE >/dev/null 2>&1
-    sleep 2
-    curl http://$IOB_SIMPLEAPI_DOMAIN:$IOB_SIMPLEAPI_PORT/set/$OID_EMAIL_TEXT?value=$EMAIL_TEXT_FALSE >/dev/null 2>&1
-    exit 0
-  fi
+    mount $MNT
+    if mountpoint -q $MNT
+      then
+        $RSYNC
+      else
+        exit 0
+    fi
 fi
 
 # EOF
