a264ee5be87498d090dc3c57b2ec7c1fe982742a
Thorsten Walk <darkiop@gmail.com>
2023-10-02 16:58:09 +0200

    echos

--- files/hosts/pve-ct-iobroker/restart-smartmeter.sh
+++ files/hosts/pve-ct-iobroker/restart-smartmeter.sh
@@ -4,13 +4,16 @@ if [ "$HOSTNAME" = pve-ct-iobroker ]; then
 
   echo "hostname = pve-ct-iobroker"
 
+  echo "ssh to loki and do a restart of ser2net.service ..."
   ssh darkiop@loki bash -c "'sudo /usr/bin/systemctl restart ser2net.service'"
 
+  echo "restarting socat-loki-usb[0,1].service ..."
   sudo systemctl restart socat-loki-usb0.service
   sudo systemctl restart socat-loki-usb1.service
 
   sleep 5
 
+  echo  "restarting ioBroker Adapter smartmeter.[0,1]"
   iobroker restart smartmeter.0
   iobroker restart smartmeter.1
 
@@ -18,13 +21,16 @@ elif [ "$HOSTNAME" = loki ]; then
 
   echo "hostname = loki"
 
+  echo "restart ser2net.service ..."
   sudo systemctl restart ser2net.service
 
+  echo "ssh to pve-ct-iobroker and do a restart of socat-loki-usb[0,1].service ..."
   ssh darkiop@pve-ct-iobroker bash -c "'sudo systemctl restart socat-loki-usb0.service'"
   ssh darkiop@pve-ct-iobroker bash -c "'sudo systemctl restart socat-loki-usb1.service'"
 
   sleep 5
-
+  
+  echo "ssh to pve-ct-iobroker and do a restart of ioBroker Adapter smartmeter.[0,1]"
   ssh darkiop@pve-ct-iobroker bash -c "'iobroker restart smartmeter.0'"
   ssh darkiop@pve-ct-iobroker bash -c "'iobroker restart smartmeter.1'"
 
@@ -32,13 +38,16 @@ else
 
   echo "hostname != pve-ct-iobroker"
 
+  echo "ssh to loki and do a restart of ser2net.service ..."
   ssh darkiop@loki bash -c "'sudo /usr/bin/systemctl restart ser2net.service'"
 
+  echo "ssh to pve-ct-iobroker and do a restart of socat-loki-usb[0,1].service ..."
   ssh darkiop@pve-ct-iobroker bash -c "'sudo systemctl restart socat-loki-usb0.service'"
   ssh darkiop@pve-ct-iobroker bash -c "'sudo systemctl restart socat-loki-usb1.service'"
 
   sleep 5
 
+  echo "ssh to pve-ct-iobroker and do a restart of ioBroker Adapter smartmeter.[0,1]"
   ssh darkiop@pve-ct-iobroker bash -c "'iobroker restart smartmeter.0'"
   ssh darkiop@pve-ct-iobroker bash -c "'iobroker restart smartmeter.1'"
 
